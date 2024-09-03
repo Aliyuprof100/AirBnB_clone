42646d21f6e499439a553ed368f69c0118f99c25
Aliyuprof <aliyuddeen95@gmail.com>
2024-09-04 00:40:07 +0100

    Console file adde

---
+++ console.py
@@ -0,0 +1,11 @@
+#/usr/bin/python3
+import cmd 
+
+class command(cmd.Cmd):
+    """This is a simple command line interpreter"""
+
+    def do_save(self, line):
+        print("Please save this file succusssfully")
+
+if __name__ == '__Main__':
+    command.cmdloop()
