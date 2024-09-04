27659b7cb234e514a4d0bb264863d22b4e96f498
aliyuprof100 <aliyuddeen95gmail.com>
2024-09-04 02:14:00 -0700

    base model activated

---
+++ models/base_model.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Import the necessary model class """
+import models
+from uuid import uuid4
+from datetime import datetime
+
+
+class Basemodel():
+    """This is tghe base models class"""
+    def __init__(self, *args, **kwargs):
+        """Instantiate a new Basemodel class".
+
+
+        args:
+            *args: Unused.
+            **kwars (dict): Key value pairs of attributes.
+        """
+    
