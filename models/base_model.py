5f76295c848176f5c758b80a2872793e0e077d79
aliyuprof100 <aliyuddeen95gmail.com>
2024-09-13 04:00:42 -0700

    BaseModel.py, init file, city file and console file edited

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""Import the necessary model class """
+
+
+"""This is the file that defines the basemodel class
+whill serve as our base model for this console project
+"""
 import models
 from uuid import uuid4
 from datetime import datetime
@@ -15,4 +19,20 @@ class Basemodel():
             *args: Unused.
             **kwars (dict): Key value pairs of attributes.
         """
-    
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
+    def save(self):
+        self.updated_at = datetime.now
+        self.storage = storage
+
+
